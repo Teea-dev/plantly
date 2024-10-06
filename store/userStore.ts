359cccebaf25fd9e515a00acb6942b11131d4c90
Adetokunbo Ogunnoiki <adetokunboogunnoiki28@gmail.com>
2024-10-06 23:48:24 +0100

    feat : state management with zustand

---
+++ store/userStore.ts
@@ -0,0 +1,14 @@
+import { create } from "zustand";
+
+type UserStore = {
+  hasFinishedOnboarding: boolean;
+  toggleHadOnboarded: () => void;
+};
+
+export const useUserStore = create<UserStore>((set) => ({
+  hasFinishedOnboarding: false,
+  toggleHadOnboarded: () =>
+    set((state) => {
+      return { ...state, hasFinishedOnboarding: !state.hasFinishedOnboarding };
+    }),
+}));
