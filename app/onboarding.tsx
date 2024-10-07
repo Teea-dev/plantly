86c19c65b6fe14f69965d2de08c4b4b314b077b8
Adetokunbo Ogunnoiki <adetokunboogunnoiki28@gmail.com>
2024-10-07 21:40:04 +0100

    feat: handling state

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,10 +1,18 @@
-import { Text, View, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { theme } from "@/theme";
-
+import { useUserStore } from "@/store/userStore";
+import { useRouter } from "expo-router";
+import { PlantlyButton } from "@/components/plantlyButton";
 export default function OnboardingScreen() {
+  const toggleHadOnboarded = useUserStore((state) => state.toggleHadOnboarded);
+  const router = useRouter();
+  const handlePress = () => {
+    toggleHadOnboarded();
+    router.replace("/");
+  };
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Onboarding</Text>
+      <PlantlyButton title="Finish onboarding" onPress={handlePress} />
     </View>
   );
 }
@@ -15,8 +23,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: theme.colorWhite,
-  },
-  text: {
-    fontSize: 24,
   },
 });
--- store/userStore.ts
+++ store/userStore.ts
@@ -1,14 +1,27 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 type UserStore = {
   hasFinishedOnboarding: boolean;
   toggleHadOnboarded: () => void;
 };
 
-export const useUserStore = create<UserStore>((set) => ({
-  hasFinishedOnboarding: false,
-  toggleHadOnboarded: () =>
-    set((state) => {
-      return { ...state, hasFinishedOnboarding: !state.hasFinishedOnboarding };
+export const useUserStore = create(
+  persist<UserStore>(
+    (set) => ({
+      hasFinishedOnboarding: false,
+      toggleHadOnboarded: () =>
+        set((state) => {
+          return {
+            ...state,
+            hasFinishedOnboarding: !state.hasFinishedOnboarding,
+          };
+        }),
     }),
-}));
+    {
+      name: "Plantly-user-store",
+      storage: createJSONStorage<UserStore>(() => AsyncStorage),
+    },
+  ),
+);
